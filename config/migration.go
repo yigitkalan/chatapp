20d88e3f15626b40e4a1f11afa7065cb40f6dbaa
Samet Yigitkalan <sametykalan@gmail.com>
2025-03-09 02:49:52 +0300

    refactor: seperate migration

--- config/migration.go
+++ config/migration.go
@@ -2,18 +2,23 @@ package config
 
 import (
 	"database/sql"
-	"time"
 	"github.com/yigitkalan/chatapp/database"
 	m "github.com/yigitkalan/chatapp/internal/message"
+	u "github.com/yigitkalan/chatapp/internal/user"
+	"time"
 )
 
 func Migrate() {
 
 	db := database.Connect()
 
-	db.AutoMigrate(&m.Message{})
+	db.AutoMigrate(&m.Message{}, &u.User{})
 
-    //Seed Data
+}
+
+func FeedSeed() {
+
+	//Seed Data
+	db := database.Connect()
 	db.Create(&m.Message{Text: "deneme", Time: sql.NullTime{Time: time.Now()}})
-
 }

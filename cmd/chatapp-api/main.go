bd832775d9d0fb5f91f2dbd07a9c7bc63892fb67
Samet Yigitkalan <sametykalan@gmail.com>
2025-03-09 03:18:44 +0300

    feat: first api endpoints

--- cmd/chatapp-api/main.go
+++ cmd/chatapp-api/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/yigitkalan/chatapp/api"
 )
 
 func main() {
+	router := gin.Default()
+    router.HandleMethodNotAllowed = true
+
+	router.GET("/api/messages", api.GetAllMessagesHandler)
+    router.GET("/api/messages/:id", api.GetMessageByIdHandler)
+
+	router.Run()
 
 }
 
